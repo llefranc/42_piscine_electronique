37d8e86c9da656bd1e8895bc6acd7822257b271d
Lucas Lefrancq <lucas.lefrancq@hotmail.fr>
2023-03-14 16:40:03 +0100

    better ex05

--- D00/ex05/main.c
+++ D00/ex05/main.c
@@ -6,27 +6,30 @@
 /*   By: llefranc <llefranc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 09:56:17 by llefranc          #+#    #+#             */
-/*   Updated: 2023/03/14 12:58:02 by llefranc         ###   ########.fr       */
+/*   Updated: 2023/03/14 16:39:55 by llefranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <avr/io.h>
 #include <util/delay.h>
 
-void update_leds(int8_t nb)
+static inline void update_leds(int8_t nb)
 {
+	nb %= 16;
 	PORTB &= ~(1 << PB4) & (0xFF << 3); /* resetting PB0/1/2/4 */
 	PORTB |= nb & ~(0xFF << 3);         /* setting PB0/1/2 */
 	PORTB |= (nb & (1 << 3)) << 1;      /* setting PB4 */
 }
 
-uint8_t update_nb(int8_t *nb, int8_t op, uint8_t is_pressed, uint8_t reg,
-                  uint8_t pin_n)
+uint8_t update_nb(int8_t op, uint8_t is_pressed, uint8_t reg, uint8_t pin_n)
 {
+	static uint8_t nb = 0;
+
 	if (!is_pressed && !(reg & (1 << pin_n))) {
 		is_pressed = 1;
 		_delay_ms(20);
-		*nb += op;           /* adding +1 or -1 */
+		nb += op;           /* adding +1 or -1 */
+		update_leds(nb);
 	} else if (is_pressed && (reg & (1 << pin_n))) {
 		is_pressed = 0;
 		_delay_ms(20);
@@ -43,19 +46,10 @@ int main(void)
 
 	uint8_t sw1_is_pressed = 0;
 	uint8_t sw2_is_pressed = 0;
-	int8_t nb = 0;
 
 	DDRB |= (1 << PB4) | (1 << PB2) | (1 << PB1) | (1 << PB0);
 	while (1) {
-
-		sw1_is_pressed = update_nb(&nb, 1, sw1_is_pressed, PIND, PD2);
-		sw2_is_pressed = update_nb(&nb, -1, sw2_is_pressed, PIND, PD4);
-
-		if (nb > 0b1111)
-			nb = 0;
-		else if (nb < 0)
-			nb = 0b1111;
-
-		update_leds(nb);
+		sw1_is_pressed = update_nb(1, sw1_is_pressed, PIND, PD2);
+		sw2_is_pressed = update_nb(-1, sw2_is_pressed, PIND, PD4);
 	}
 }
