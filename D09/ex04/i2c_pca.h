52f4fe32ad15129e698cec83c0b8de42bc4b9af0
Lucas Lefrancq <lucas.lefrancq@hotmail.fr>
2023-03-27 11:25:27 +0200

    ex04 done

---
+++ D09/ex04/i2c_pca.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   i2c_pca.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llefranc <llefranc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/26 20:51:58 by llefranc          #+#    #+#             */
+/*   Updated: 2023/03/26 20:58:11 by llefranc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef I2C_PCA_H
+#define I2C_PCA_H
+
+#include "i2c.h"
+#include <avr/io.h>
+
+#define I2C_PCA_SLA 0b0100000
+
+/* Command byte */
+#define I2C_PCA_I0 0
+#define I2C_PCA_I1 1
+#define I2C_PCA_O0 2
+#define I2C_PCA_O1 3
+#define I2C_PCA_N0 4
+#define I2C_PCA_N1 5
+#define I2C_PCA_C0 6
+#define I2C_PCA_C1 7
+
+/* IO pins mapping for register 0 */
+#define I2C_PCA0_SW5 0
+#define I2C_PCA0_D11 1
+#define I2C_PCA0_D10 2
+#define I2C_PCA0_D9 3
+#define I2C_PCA0_DIG1 4
+#define I2C_PCA0_DIG2 5
+#define I2C_PCA0_DIG3 6
+#define I2C_PCA0_DIG4 7
+
+/* IO pins mapping for register 1 */
+#define I2C_PCA1_A 0
+#define I2C_PCA1_B 1
+#define I2C_PCA1_C 2
+#define I2C_PCA1_D 3
+#define I2C_PCA1_E 4
+#define I2C_PCA1_F 5
+#define I2C_PCA1_G 6
+#define I2C_PCA1_DPX 7
+
+void i2c_pca_write_reg(uint8_t reg, uint8_t data);
+uint8_t i2c_pca_read_reg(uint8_t reg);
+int8_t i2c_pca_draw_seg_nb(uint8_t nb, uint8_t dig,  uint8_t dx,
+			   uint8_t pcaO0_bits_save);
+
+#endif /* I2C_PCA_H */
---
+++ D09/ex04/main.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llefranc <llefranc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/26 20:52:35 by llefranc          #+#    #+#             */
+/*   Updated: 2023/03/27 11:24:32 by llefranc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "i2c.h"
+#include "i2c_pca.h"
+#include "timer.h"
+
+#include <avr/io.h>
+#include <util/twi.h>
+#include <avr/interrupt.h>
+
+ISR(TIMER1_COMPA_vect)
+{
+	static uint8_t switch_nb = 0;
+
+	if (!switch_nb)
+		i2c_pca_draw_seg_nb(4, (1 << I2C_PCA0_DIG3), 0, 0);
+	else
+		i2c_pca_draw_seg_nb(2, (1 << I2C_PCA0_DIG4), 1, 0);
+	switch_nb = !switch_nb;
+}
+
+int main(void)
+{
+	sei();
+	i2c_init();
+
+	/* All segments pins in output */
+	i2c_pca_write_reg(I2C_PCA_C0, 0xF0);
+	i2c_pca_write_reg(I2C_PCA_C1, 0xFF);
+
+	start_timer1_ms(10);
+	while (1);
+}
